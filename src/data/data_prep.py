7a9a9ec705754e33b77f16aaa2042e2837ac1302
Uma Maheswara Rao Meleti <maheshmeleti350@gmail.com>
2024-11-02 11:30:53 -0400

    added pipeline with median and 750 estimators

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -12,12 +12,12 @@ def load_data(filepath : str) -> pd.DataFrame:
 # test_data = pd.read_csv("./data/raw/test.csv")
 
 
-def fill_missing_with_mean(df):
+def fill_missing_with_median(df):
     try:
         for column in df.columns:
             if df[column].isnull().any():
-                mean_value = df[column].mean()
-                df[column].fillna(mean_value,inplace=True)
+                median_value = df[column].median()
+                df[column].fillna(median_value,inplace=True)
         return df
     except Exception as e:
         raise Exception(f"Error Filling missing values with mean:{e}")
@@ -39,8 +39,8 @@ def main():
         train_data = load_data(os.path.join(raw_data_path,"train.csv"))
         test_data = load_data(os.path.join(raw_data_path,"test.csv"))
 
-        train_processed_data = fill_missing_with_mean(train_data)
-        test_processed_data = fill_missing_with_mean(test_data)
+        train_processed_data = fill_missing_with_median(train_data)
+        test_processed_data = fill_missing_with_median(test_data)
 
 
         os.makedirs(processed_data_path, exist_ok=True)
